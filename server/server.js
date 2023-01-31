65a868130a78de6f165e0ab9964d47136c50cd07
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2023-01-31 00:08:38 -0500

    setting up server and fetch data from pgSql

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const PORT = process.env.PORT ?? 8000;
+const express = require("express");
+const app = express();
+const pool = require("./db");
+
+//get all todos
+app.get("/todos", async (req, res) => {
+  const userEmail = " rama@yahoo.com";
+  try {
+    const todos = await pool.query(
+      "SELECT * FROM todos WHERE user_email = $1",
+      [userEmail]
+    );
+    res.json(todos.rows);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
