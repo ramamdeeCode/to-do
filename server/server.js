72794b9661f9f61edf6761ca4d71c399dd2862a8
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2023-01-31 08:35:50 -0500

    add cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const PORT = process.env.PORT ?? 8000;
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const pool = require("./db");
 
+app.use(cors());
+
 //get all todos
-app.get("/todos", async (req, res) => {
-  const userEmail = " rama@yahoo.com";
+app.get("/todos/:userEmail", async (req, res) => {
+  //console.log(req);
+  const { userEmail } = req.params;
   try {
     const todos = await pool.query(
       "SELECT * FROM todos WHERE user_email = $1",
