0aba37555d4bcdd516283c4778c5565004551bf1
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2023-01-31 08:37:22 -0500

    Impement the listItem class and some css

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,5 +1,21 @@
-function ListItem() {
-  return <div className="App">Rama</div>;
+import TickIcon from "./TickIcon";
+import ProgressBar from "./ProgressBar";
+
+function ListItem({ task }) {
+  return (
+    <li className="list-item">
+      <div className="info-conctainer">
+        <TickIcon />
+        <p className="task-title">{task.title}</p>
+        <ProgressBar />
+      </div>
+
+      <div className="button-container">
+        <button className="edit">Edit</button>
+        <button className="delete">DELETE</button>
+      </div>
+    </li>
+  );
 }
 
 export default ListItem;
