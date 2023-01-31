5405beaeb23f3d391bc8feebb5beacf399b9dafa
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2023-01-31 08:36:51 -0500

    add useEffect and useState to fetch data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
 import ListHeader from "./components/ListHeader";
+import { useState, useEffect } from "react";
+import ListItem from "./components/ListItem";
 
 function App() {
+  const userEmail = " rama@yahoo.com";
+  const [task, setTasks] = useState(null);
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const data = await response.json();
+
+      setTasks(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => getData, []);
+  console.log(task);
+
+  const sortTaskByDate = task?.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
   return (
     <div className="app">
       <ListHeader listName={"🏝️ Holiday tick list"} />
+      {sortTaskByDate?.map((task) => (
+        <ListItem key={task.id} task={task} />
+      ))}
     </div>
   );
 }
